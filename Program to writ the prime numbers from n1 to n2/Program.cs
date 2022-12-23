585b94c21061f66eaa187282ee003e72c3b7aa10
karim3bdel3azim <121309649+karim3bdel3azim@users.noreply.github.com>
2022-12-23 19:06:00 +0200

    Add files via upload

---
+++ Program to writ the prime numbers from n1 to n2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+class PrimeNumber
+{
+    static public void Main()
+    {
+
+        int a, b, i, j, flag;
+
+        Console.Write(
+        "Enter lower bound of the interval: ");
+        a = Convert.ToInt32(
+        Console.ReadLine()); 
+
+        Console.Write(
+        "\nEnter upper bound of the interval: ");
+        b = Convert.ToInt32(
+        Console.ReadLine());
+
+        Console.Write("\nPrime numbers between " + a
+                    + " and " + b + " are: ");
+
+        if (a == 1)
+        {
+            Console.Write(a + " ");
+            a++;
+            if (b >= 2)
+            {
+                Console.Write(a + " ");
+                a++;
+            }
+        }
+        if (a == 2)
+        {
+            Console.Write(a + " ");
+        }
+
+        if (a % 2 == 0)
+        {
+            a++;
+        }
+
+        for (i = a; i <= b; i = i + 2)
+        {
+
+            flag = 1;
+
+            for (j = 2; j * j <= i; ++j)
+            {
+                if (i % j == 0)
+                {
+                    flag = 0;
+                    break;
+                }
+            }
+
+            if (flag == 1)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
+
